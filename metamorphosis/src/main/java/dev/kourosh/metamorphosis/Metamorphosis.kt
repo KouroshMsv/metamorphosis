2f4428c8e05dbff073f0002819f6a99d5e3fc447
Kourosh <mousavi.card@gmail.com>
2020-01-18 17:09:56 +0330

    change downloader thread

--- metamorphosis/src/main/java/dev/kourosh/metamorphosis/Metamorphosis.kt
+++ metamorphosis/src/main/java/dev/kourosh/metamorphosis/Metamorphosis.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.launch
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-class Metamorphosis(val builder: Builder) {
+class Metamorphosis(private val builder: Builder) {
 
 
     var downloadListener: OnDownloadListener? = null
@@ -27,7 +27,7 @@ class Metamorphosis(val builder: Builder) {
     }
 
     fun checkVersion(checkVersionListener: OnCheckVersionListener) {
-        GlobalScope.launch {
+        GlobalScope.launch(Dispatchers.IO) {
             when (val res = CheckVersion(builder.versionCheckerUrl).execute()
                 .get(builder.timeOut, TimeUnit.MILLISECONDS).await()) {
                 is Result.Success -> {
@@ -42,7 +42,7 @@ class Metamorphosis(val builder: Builder) {
                 }
                 else -> {
                     launch(Dispatchers.Main) {
-                        checkVersionListener?.onFailed("Time out", null)
+                        checkVersionListener.onFailed("Time out", null)
 
                     }
                 }
@@ -50,42 +50,48 @@ class Metamorphosis(val builder: Builder) {
         }
     }
 
-    fun startDownload(apkUrl:String) {
+    fun startDownload(apkUrl: String) {
         try {
-            builder.run {
-                if (!apkName.contains(".apk")){
-                    apkName+=".apk"
-                }
-                val file = File(dir, apkName)
-                if (file.exists())
-                    file.delete()
-                val request = DownloadManager.Request(Uri.parse(apkUrl))
-                    .setTitle(notificationConfig.title)
-                    .setDescription(notificationConfig.description)
-                    .setNotificationVisibility(notificationConfig.notificationVisibility)
-                    .setDestinationUri(Uri.fromFile(file))
+            GlobalScope.launch(Dispatchers.IO) {
 
-                val downloadManager =activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
-                val downloadId = downloadManager!!.enqueue(request)
-                var downloading = true
-                var lastPercent = -1
-                while (downloading) {
-                    val q = DownloadManager.Query()
-                    q.setFilterById(downloadId)
-                    val cursor = downloadManager.query(q)
-                    cursor.moveToFirst()
-                    val bytesDownloaded= cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
-                    val bytesTotal=cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
-                    val percent = ((bytesDownloaded * 100L) / bytesTotal).toInt()
-                    if (percent != lastPercent) {
-                        downloadingListener?.downloading(percent)
-                        lastPercent = percent
+                builder.run {
+                    if (!apkName.contains(".apk")) {
+                        apkName += ".apk"
                     }
-                    if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
-                        downloading = false
-                        downloadListener?.onFinished(file)
+                    val file = File(dir, apkName)
+                    if (file.exists())
+                        file.delete()
+                    val request = DownloadManager.Request(Uri.parse(apkUrl))
+                        .setTitle(notificationConfig.title)
+                        .setDescription(notificationConfig.description)
+                        .setNotificationVisibility(notificationConfig.notificationVisibility)
+                        .setDestinationUri(Uri.fromFile(file))
+
+                    val downloadManager =
+                        activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
+                    val downloadId = downloadManager!!.enqueue(request)
+                    var downloading = true
+                    var lastPercent = -1
+                    while (downloading) {
+                        val q = DownloadManager.Query()
+                        q.setFilterById(downloadId)
+                        val cursor = downloadManager.query(q)
+                        cursor.moveToFirst()
+                        val bytesDownloaded =
+                            cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                        val bytesTotal =
+                            cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+                        val percent = ((bytesDownloaded * 100L) / bytesTotal).toInt()
+                        if (percent != lastPercent) {
+                            downloadingListener?.downloading(percent)
+                            lastPercent = percent
+                        }
+                        if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                            downloading = false
+                            downloadListener?.onFinished(file)
+                        }
+                        cursor.close()
                     }
-                    cursor.close()
                 }
             }
 
@@ -96,11 +102,12 @@ class Metamorphosis(val builder: Builder) {
     }
 
 
-
-    fun installAPK( file: File) {
+    fun installAPK(file: File) {
         val intent = Intent(Intent.ACTION_VIEW)
         val fileUri = FileProvider.getUriForFile(
-            builder.activity,   builder.activity.applicationContext.packageName.toString() + ".metamorphosisProvider", file
+            builder.activity,
+            builder.activity.applicationContext.packageName.toString() + ".metamorphosisProvider",
+            file
         )
         intent.setDataAndType(fileUri, "application/vnd.android.package-archive")
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
