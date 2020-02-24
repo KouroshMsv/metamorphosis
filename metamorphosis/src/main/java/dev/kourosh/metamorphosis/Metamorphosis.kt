adcaabf6de74340bee0cfa74cad38d25e6962d21
Kourosh <mousavi.card@gmail.com>
2020-02-24 17:40:26 +0330

    change installer

--- metamorphosis/src/main/java/dev/kourosh/metamorphosis/Metamorphosis.kt
+++ metamorphosis/src/main/java/dev/kourosh/metamorphosis/Metamorphosis.kt
@@ -4,6 +4,7 @@ import android.app.DownloadManager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.util.Log
 import androidx.core.content.FileProvider
 import kotlinx.coroutines.Dispatchers
@@ -103,17 +104,26 @@ class Metamorphosis(val builder: Builder) {
 
 
     fun installAPK(file: File) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        val fileUri = FileProvider.getUriForFile(
-            builder.activity,
-            builder.activity.applicationContext.packageName.toString() + ".metamorphosisProvider",
-            file
+        builder.activity.startActivity(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                val apkUri = FileProvider.getUriForFile(
+                    builder.activity,
+                    builder.activity.applicationContext.packageName.toString() + ".metamorphosisProvider",
+                    file
+                )
+                val intent = Intent(Intent.ACTION_INSTALL_PACKAGE)
+                intent.data = apkUri
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                intent
+            } else {
+                val apkUri = Uri.fromFile(file)
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                intent
+            }
         )
-        intent.setDataAndType(fileUri, "application/vnd.android.package-archive")
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        builder.activity.startActivity(intent)
     }
-
 
 }
 
